8e7961c66de79634f28bda9ae66176b7d6ec4c5e
Oleksii Korolenko <as_vostok@i.ua>
2023-03-16 22:52:43 +0200

    add task 3

--- src/components/friends/FriendListItem.jsx
+++ src/components/friends/FriendListItem.jsx
@@ -3,7 +3,7 @@ import css from './Friends.module.css';
 export default function FriendListItem({ avatar, name, isOnline }) {
   return (
     <>
-      <span className={css.status}></span>
+      <span className={css[`${isOnline ? 'activ' : 'inactiv'}`]}></span>
       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={css.name}>{name}</p>
     </>
